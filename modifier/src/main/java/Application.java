569d5de559e3a35e11ddda3cd5c95b544a5532eb
GraceeFlower <1491644103@qq.com>
2020-02-11 21:12:37 +0800

    [WM] feat: create two students & their teachers

--- modifier/src/main/java/Application.java
+++ modifier/src/main/java/Application.java
@@ -1,7 +1,11 @@
 public class Application {
 
   public static void main(String[] args) {
+    Student Mary = new Student("Mary", 80);
+    Student Cindy = new Student("Cindy", 95);
+
+    Teacher Solider = new Teacher("Solider", Mary);
+    Teacher Bob = new Teacher("Bob", Cindy);
 
   }
-
 }
